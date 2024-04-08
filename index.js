b9483943045563c7f16dde87859f34590e3d5eb3
Luca de Alfaro <luca@dealfaro.org>
2024-04-08 13:27:18 -0700

    Starting point for Vue development

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let app = {};
+
+app.data = {    
+    data: function() {
+        return {
+        };
+    },
+    computed: {
+    },
+    methods: {
+    }
+};
+
+app.vue = Vue.createApp(app.data).mount("#app");
